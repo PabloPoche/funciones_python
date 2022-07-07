5c92d79e6408dc3aa814fb64f1140a752020f60a
PabloPoche <107163780+PabloPoche@users.noreply.github.com>
2022-07-07 07:39:52 -0300

    Update ejercicio_5.py

--- ejercicios_practica/ejercicio_5.py
+++ ejercicios_practica/ejercicio_5.py
@@ -42,15 +42,17 @@ if __name__ == '__main__':
 
     # 1) Primero generar una lista de invitados con "generar_invitados"
     #    Almacenar el resultado en "lista_invitados"
-
+    
     # lista_invitados = generar_invitados()
-
+    lista_invitados = generar_invitados()
+    
     # 2) Luego ordenar la lista de invitados con "ordenar"
     #    --> Pasar como parámetro la "lista_invitados"
     #    --> Retornar la lista de invitados ordenada
 
     # lista_invidatos_ordenada = ordenar(lista_invitados)
+    lista_invidatos_ordenada = ordenar(lista_invitados)
 
     # Imprimir en pantalla "lista_invidatos_ordenada":
-
+    print(lista_invidatos_ordenada)
     print("terminamos")
